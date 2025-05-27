211947cfe4af9bacb4511a481983766c9f3c24fa
Jerry-89 <kontakt.jerry@gmail.com>
2025-05-27 19:08:36 +0200

    first comit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+    //Toto je komentar
+
+        String pozdrav = "Hello world";
+        System.out.println (pozdrav);
+
+    }
+}
