784a91b3ad67ce52a7cf1d411028c9a85abe7371
Jerry-89 <kontakt.jerry@gmail.com>
2025-05-29 18:09:12 +0200

    dalsi

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,22 @@
-import java.util.Scanner; // <- toto si pridal
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+/*
+        int aktRok = 2025;
 
-                //komentar
-                int aktRok = 2025;
+        Scanner mujScanner = new Scanner(System.in);
 
-                Scanner mujScanner = new Scanner(System.in);
+        System.out.println("Zadaj svoj rok narodenia: ");
+        int rokNar = mujScanner.nextInt();
 
-                System.out.println("Zadaj svoj rok narodenia: ");
-                int rokNar = mujScanner.nextInt();
-
-                int vek = aktRok - rokNar;
-                System.out.println("Tvoj vek je " + vek);
-        }
-    }
+        int vek = aktRok - rokNar;
+        if (rokNar <= aktRok) {
+            System.out.println("Tvoj vek je " + vek);
+        } else
+*/
+                    System.out.println("Nespravny vek");
+     /*   Scanner mujScanner = new Scanner(System.in);
+        System.out.println("Zadaj svoj email: ");
+        String email = mujScanner.nextLine();
+        System.out.println(email.toLowerCase()); */
